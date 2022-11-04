6ae61f3d14b7a464d7a410a61e5fc56d777dd102
somchai2647 <somchai.b@cvc.ac.th>
2022-11-04 10:40:01 +0700

    success

--- components/CompanyNavbar.jsx
+++ components/CompanyNavbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function CompanyNavbar({ name }) {
     return (
         <div className='flex drop-shadow-xl bg-opacity-50 justify-center '>
-            <img
-                src={`/assets/company/${name=="J.C.STAFF"? "J C STAFF" : name}.png`}
-                alt="companybrand"
-                style={{ width: "20rem" }}
-            />
+            <Link to={"/"} title="HomeScreen">
+                <img
+                    src={`/assets/company/${name == "J.C.STAFF" ? "J C STAFF" : name}.png`}
+                    alt="companybrand"
+                    style={{ width: "20rem" }}
+                />
+            </Link>
+
         </div>
     )
 }
